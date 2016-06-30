6ad1fb62defd8a0d00f5fcc891609986a1d8ec0b
emil.skawn <namju@fff5562e8198>
2016-06-30 11:02:43 +0000

    before adopting django model

---
+++ yacc/urls.py
@@ -0,0 +1,25 @@
+"""yacc URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', 'yacc.views.index'),
+    url(r'^func/$', 'yacc.views.func'),
+    url(r'^func/(?P<call_string>.+)$', 'yacc.views.func'),
+    url(r'^func_readable/(?P<call_string>.+)$', 'yacc.views.func_readable'),
+]
